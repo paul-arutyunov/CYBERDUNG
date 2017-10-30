187fe21bdec2754a41ad4c7a5d56e4ef95abac43
Paul_Arutyunov <pashaarutyunov@gmail.com>
2017-10-30 15:33:24 +0300

    Made some changes

--- include/monsters.h
+++ include/monsters.h
@@ -1,9 +1,14 @@
 #ifndef _MONSTERS_H_
 #define _MONSTERS_H_
 
+#define MONSTER_TAB_SIZE (sizeof monster_tab / sizeof (Monster) )
+
 typedef struct {
+	char *name;
 	int hp;
 	int str;
 } Monster;
+
+extern Monster monster_tab[];
 
 #endif
